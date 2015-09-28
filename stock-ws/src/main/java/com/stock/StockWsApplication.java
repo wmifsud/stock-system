16d531fe9083fa53474d3321937ef1265fc57d06
wmifsud <waylonmifsud@gmail.com>
2015-09-28 21:53:41 +0200

    Program structure creation
    Main project being stock-system having:
    stock-ws responsible for REST ws and polling queue,
    stock-data responsible for database stuff.

---
+++ stock-ws/src/main/java/com/stock/StockWsApplication.java
@@ -0,0 +1,12 @@
+package com.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockWsApplication.class, args);
+    }
+}
